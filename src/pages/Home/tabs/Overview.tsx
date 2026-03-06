29963a516d6e8730966005d92d893bdf7d973a8a
talkohavy <51020442+talkohavy@users.noreply.github.com>
2026-03-06 09:39:46 +0200

    small

--- src/pages/Home/tabs/Overview.tsx
+++ src/pages/Home/tabs/Overview.tsx
@@ -18,18 +18,18 @@ export default function Overview() {
 
       <div className='mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
         <div className='p-4 border rounded-lg dark:border-gray-600'>
-          <h3 className='font-semibold'>Feature 1</h3>
-          <p className='text-sm text-gray-500'>Description of feature 1</p>
+          <h3 className='font-semibold'>Box 1</h3>
+          <p className='text-sm text-gray-500'>Description of Box 1</p>
         </div>
 
         <div className='p-4 border rounded-lg dark:border-gray-600'>
-          <h3 className='font-semibold'>Feature 2</h3>
-          <p className='text-sm text-gray-500'>Description of feature 2</p>
+          <h3 className='font-semibold'>Box 2</h3>
+          <p className='text-sm text-gray-500'>Description of Box 2</p>
         </div>
 
         <div className='p-4 border rounded-lg dark:border-gray-600'>
-          <h3 className='font-semibold'>Feature 3</h3>
-          <p className='text-sm text-gray-500'>Description of feature 3</p>
+          <h3 className='font-semibold'>Box 3</h3>
+          <p className='text-sm text-gray-500'>Description of Box 3</p>
         </div>
       </div>
     </div>
