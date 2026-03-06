79e6594cc9670ae78542170f511b1f201366aeda
talkohavy <51020442+talkohavy@users.noreply.github.com>
2026-03-06 03:04:23 +0200

    disable https ssl

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import tailwindcss from '@tailwindcss/vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+// import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig, loadEnv } from 'vite';
 import svgr from 'vite-plugin-svgr';
@@ -25,7 +25,7 @@ export default defineConfig(({ mode }) => {
     root: `${process.cwd()}/src`, // <--- defaults to process.cwd(). where the index.html is located.
     plugins: [
       react(),
-      basicSsl(), // <--- Enable this to have HTTPS in dev
+      // basicSsl(), // <--- Enable this to have HTTPS in dev
       tailwindcss(),
       svgr({
         include: '**/*.svg',
